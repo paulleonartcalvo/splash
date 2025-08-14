3fa7cb261bce8d5b2e37d279737e62ac10316587
paulleonartcalvo <plleonartcalvo@gmail.com>
2025-08-15 00:19:54 +0200

    feat: init

--- apps/splash/src/routes/index.tsx
+++ apps/splash/src/routes/index.tsx
@@ -1,9 +1,17 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import Splash from "../assets/splash.svg?react";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return (
+    <div className="flex flex-col justify-center items-center h-full w-full gap-16">
+      <Splash height="96px" className="text-teal-400" />
+      <h1 className="scroll-m-20 text-center text-4xl font-extrabold ">
+        Pool reservations made easy
+      </h1>
+    </div>
+  );
 }
