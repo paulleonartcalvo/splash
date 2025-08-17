8fbc66143c2a4cc684f4b26d936ed781cfc16bc4
paulleonartcalvo <plleonartcalvo@gmail.com>
2025-08-17 21:23:42 +0200

    feat: start work on location details

---
+++ apps/splash/src/components/LocationDetails.tsx
@@ -0,0 +1,12 @@
+import type { Location } from "@/services/location/queries";
+
+type LocationDetailsProps = {
+  location: Location;
+};
+
+export function LocationDetails({ location }: LocationDetailsProps) {
+  return <div className="w-full h-full ">
+
+
+  </div>;
+}
